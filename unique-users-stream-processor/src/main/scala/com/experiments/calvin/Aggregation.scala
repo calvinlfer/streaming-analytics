746ffac0eea2bc4a5ce99fe7a9434163fe1c9a62
calvinlfer <calvin.l.fer@gmail.com>
2018-02-25 00:30:54 -0500

    add some more tests and eliminate duplicates

--- unique-users-stream-processor/src/main/scala/com/experiments/calvin/Aggregation.scala
+++ unique-users-stream-processor/src/main/scala/com/experiments/calvin/Aggregation.scala
@@ -16,15 +16,6 @@ trait Aggregation {
     YMDH(year, month, day, hour)
   }
 
-  def countByYMDH(es: Seq[AnalyticsEvent]): List[(YMDH, Count)] = {
-    def countByYMDH(acc: Map[YMDH, Count], next: AnalyticsEvent): Map[YMDH, Count] = {
-      val ymdh: YMDH   = extractYMDH(next.timestamp)
-      val updatedCount = acc.getOrElse(ymdh, 0) + 1
-      acc + (ymdh -> updatedCount)
-    }
-    es.foldLeft(Map.empty[YMDH, Count])(countByYMDH).toList
-  }
-
   def uniqueUserIdByYMDH(es: Seq[AnalyticsEvent]): List[(YMDH, Seq[UserId])] = {
     es.groupBy(event => extractYMDH(event.timestamp))
       .mapValues(seq => seq.map(_.userId).distinct)
--- unique-users-stream-processor/src/test/scala/com/experiments/calvin/AggregationSpec.scala
+++ unique-users-stream-processor/src/test/scala/com/experiments/calvin/AggregationSpec.scala
@@ -3,6 +3,7 @@ package com.experiments.calvin
 import java.time.ZonedDateTime
 
 import com.experiments.calvin.models._
+import net.agkn.hll.HLL
 import org.scalatest.{FunSpec, MustMatchers}
 
 class AggregationSpec extends FunSpec with MustMatchers with Aggregation {
@@ -17,20 +18,6 @@ class AggregationSpec extends FunSpec with MustMatchers with Aggregation {
     }
   }
 
-  describe("countByYMDH") {
-    it("must categorize events by YMDH and then count them by their YMDH") {
-      val zdtA = ZonedDateTime.now()
-      val zdtB = zdtA.minusDays(1)
-      val es = AnalyticsEvent(zdtA, "click", "bob") :: AnalyticsEvent(zdtA, "click", "jim") ::
-      AnalyticsEvent(zdtB, "click", "john") :: Nil
-
-      val result: Seq[(YMDH, Count)] = countByYMDH(es)
-      result.length mustBe 2
-      result must contain(extractYMDH(zdtA) -> 2)
-      result must contain(extractYMDH(zdtB) -> 1)
-    }
-  }
-
   describe("uniqueUserIdByYMDH") {
     it("must come up with a list of unique usernames by YMDH") {
       val zdtA = ZonedDateTime.now()
@@ -42,6 +29,29 @@ class AggregationSpec extends FunSpec with MustMatchers with Aggregation {
       result.length mustBe 2
       result must contain(extractYMDH(zdtA) -> List("bob"))
       result must contain(extractYMDH(zdtB) -> List("john"))
+    }
+  }
+
+  describe("userEstimatesByYMDH") {
+    it("estimating cardinality should be the same as the actual cardinality for average numbers") {
+      val zdt             = ZonedDateTime.now()
+      val ymdh            = extractYMDH(zdt)
+      val userIds         = Range(0, 100).inclusive.map(_.toString)
+      val (_, hll) :: Nil = userEstimatesByYMDH(Seq(ymdh -> userIds))
+      hll.cardinality() mustBe 101
+    }
+  }
+
+  describe("mergeHLLMappings") {
+    it("merging cardinality mappings with itself should have no effect") {
+      val zdt     = ZonedDateTime.now()
+      val ymdh    = extractYMDH(zdt)
+      val userIds = Range(0, 100).inclusive.map(_.toString)
+      val result  = userEstimatesByYMDH(Seq(ymdh -> userIds)).toMap
+
+      val mergedResult: Map[YMDH, HLL] = mergeHLLMappings(result, result)
+      mergedResult.size mustBe 1
+      mergedResult(ymdh).cardinality() mustBe 101
     }
   }
 }
